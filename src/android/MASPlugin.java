989b00621d1020a7d7ef448a41c443ffd6d26b2f
Yash Sangani <sanya03@ca.com>
2016-04-22 14:19:42 -0700

    MCT-113 Cordova: Device Registration with password flow Added Android implementation for password flow, start, deregister

---
+++ src/android/MASPlugin.java
@@ -0,0 +1,98 @@
+package com.ca.apim;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.ca.mas.MAS;
+import com.ca.mas.foundation.Device;
+import com.ca.mas.foundation.MASCallback;
+import com.ca.mas.foundation.MASDevice;
+import com.ca.mas.foundation.MASUser;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.concurrent.CountDownLatch;
+
+
+public class MASPlugin extends CordovaPlugin{
+
+    private static final String TAG=MASPlugin.class.getCanonicalName();
+
+    private static final String SUCCESS="success";
+    private static final String FAIL="fail";
+    private static final String REGISTER_WITH_USER_CREDENTIALS="loginWithUsernameAndPassword";
+    private static final String START="start";
+    private static final String DEREGISTER="deregister";
+
+    @Override
+    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
+
+
+        Context context = webView.getContext();
+        Device masDevice = null;
+
+
+        if(action.equals(REGISTER_WITH_USER_CREDENTIALS)){
+
+            masDevice=new MASDevice(context);
+            final CountDownLatch latch=new CountDownLatch(1);
+            MASUser.getCurrentUser(context).login(new MASCallback<MASUser>() {
+                @Override
+                public void onSuccess(MASUser masUser) {
+                    callbackContext.success(SUCCESS);
+                    latch.countDown();
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    callbackContext.error(FAIL);
+                    latch.countDown();
+                }
+            });
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if(action.equals(START)){
+
+            try{
+                MAS.start(context);
+                callbackContext.success(SUCCESS);
+
+            }catch(Exception e){
+                Log.e(TAG,"Error while starting MAS SDK",e);
+                callbackContext.error(FAIL);
+            }
+        }
+        if(action.equals(DEREGISTER)){
+            masDevice=new MASDevice(context);
+            final CountDownLatch latch=new CountDownLatch(1);
+            masDevice.deregister(new MASCallback<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    callbackContext.success(SUCCESS);
+                    latch.countDown();
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    callbackContext.error(FAIL);
+                    latch.countDown();
+                }
+            });
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+}
+
