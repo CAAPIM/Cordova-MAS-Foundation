25cee1817a5b3048317b1aff560cf2ce6797da24
James Go <hun.go@ca.com>
2016-05-30 16:33:10 -0700

    MCT-261 Cordova iOS Implementation - add copyright headers

--- src/ios/MASPlugin.h
+++ src/ios/MASPlugin.h
@@ -1,7 +1,10 @@
 //
 //  MASPlugin.h
 //
-//  Copyright (c) 2016 CA Technologies. All rights reserved.
+//  Copyright (c) 2016 CA, Inc.
+//
+//  This software may be modified and distributed under the terms
+//  of the MIT license. See the LICENSE file for details.
 //
 
 #import <Cordova/CDV.h>
