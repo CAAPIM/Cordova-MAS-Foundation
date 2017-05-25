ff9f33434b9bcec437bc3ef4c56dc10cc809b936
Mohammed Mujeeb <mujmo02@ca.com>
2017-05-25 16:32:35 +0530

    Setting up unit test cases

--- tests/tests.js
+++ tests/tests.js
@@ -6,6 +6,25 @@
  */
 
 
+exports.defineAutoTests = function() {
+
+    describe('awesome tests', function() {
+        it('do something sync', function() {
+            expect(1).toBe(1);
+        });
+
+        it('do something async', function(done) {
+            setTimeout(function() {
+                expect(1).toBe(1);
+                done();
+            }, 100);
+        });
+    });
+
+    describe('more awesome tests', function() {});
+
+};
+
 /**
  *Below are the manual tests for the MASPlugin.
  *
@@ -13,265 +32,286 @@
  * test. As we found some issues related to time out to numerous async calls.
  */
 
+
 exports.defineManualTests = function(contentEl, createActionButton) {
 
 
     createActionButton('MAS.initialize()', function() {
-            var name = 'MAS.initialize()';
-            var mas = new MASPlugin.MAS();
-            mas.initialize(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MAS.initialize()';
+        var mas = new MASPlugin.MAS();
+        mas.initialize(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MAS.start()', function() {
-            var name = 'MAS.start()';
-            var mas = new MASPlugin.MAS();
-            mas.start(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MAS.start()';
+        var mas = new MASPlugin.MAS();
+        mas.start(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MAS.stop()', function() {
-            var name = 'MAS.stop()';
-            var mas = new MASPlugin.MAS();
-            mas.stop(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MAS.stop()';
+        var mas = new MASPlugin.MAS();
+        mas.stop(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MAS.setGrantFlow(1)', function() {
-            var name = 'MAS.setGrantFlow(1)';
-            var mas = new MASPlugin.MAS();
-            mas.grantFlow(startSuccessHandler, errorHandler, 1);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MAS.setGrantFlow(1)';
+        var mas = new MASPlugin.MAS();
+        mas.grantFlow(startSuccessHandler, errorHandler, 1);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MAS.setConfigFileName(test.json)', function() {
-            var name = 'MAS.setConfigFileName(test.json)';
-            var mas = new MASPlugin.MAS();
-            mas.configFileName(startSuccessHandler, errorHandler, 'test.json');
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MAS.setConfigFileName(test.json)';
+        var mas = new MASPlugin.MAS();
+        mas.configFileName(startSuccessHandler, errorHandler, 'test.json');
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MAS.getFromPath()', function() {
-            var name = 'MAS.getFromPath()';
-            var mas = new MASPlugin.MAS();
-            var parameters = {"operation": "listProducts"};
-            var headers = {"headerName" : "headerValue"};
-            mas.getFromPath(startSuccessHandler, errorHandler, '/protected/resource/products'
-                , parameters, headers, 0,  0);
-            function startSuccessHandler(result) {
-                console.log(name + ' - Success: ' + JSON.stringify(result));
-            }
-            function errorHandler(error) {
-                console.log(name + ' - Failed: ' + error.errorInfo);
-            }
-        });
+        var name = 'MAS.getFromPath()';
+        var mas = new MASPlugin.MAS();
+        var parameters = { "operation": "listProducts" };
+        var headers = { "headerName": "headerValue" };
+        mas.getFromPath(startSuccessHandler, errorHandler, '/protected/resource/products', parameters, headers, 0, 0);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' - Success: ' + JSON.stringify(result));
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' - Failed: ' + error.errorInfo);
+        }
+    });
 
 
     createActionButton('MAS.getFromPath() OTP', function() {
-            var name = 'MAS.getFromPath() OTP';
-            var mas = new MASPlugin.MAS();
-            var parameters = {"operation": "OTP"};
-            var headers = {"headerName" : "headerValue"};
-            mas.getFromPath(startSuccessHandler, errorHandler, '/otpProtected'
-                , parameters, headers, 0,  0);
-            function startSuccessHandler(result) {
-                console.log(name + ' - Success: ' + JSON.stringify(result));
-            }
-            function errorHandler(error) {
-                console.log(name + ' - Failed: ' + error.errorInfo);
-            }
-        });
+        var name = 'MAS.getFromPath() OTP';
+        var mas = new MASPlugin.MAS();
+        var parameters = { "operation": "OTP" };
+        var headers = { "headerName": "headerValue" };
+        mas.getFromPath(startSuccessHandler, errorHandler, '/otpProtected', parameters, headers, 0, 0);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' - Success: ' + JSON.stringify(result));
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' - Failed: ' + error.errorInfo);
+        }
+    });
 
 
     //MASUser
     createActionButton('MASUser.loginWithUsernameAndPassword()', function() {
-            var name = 'MASUser.loginWithUsernameAndPassword()';
-            var masUser = new MASPlugin.MASUser();
-            masUser.loginWithUsernameAndPassword(startSuccessHandler, errorHandler, 'sunder', 'dost1234');
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MASUser.loginWithUsernameAndPassword()';
+        var masUser = new MASPlugin.MASUser();
+        masUser.loginWithUsernameAndPassword(startSuccessHandler, errorHandler, 'sunder', 'dost1234');
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
 
     createActionButton('MASUser.logoutUser()', function() {
-            var name = 'MASUser.logoutUser()';
-            var masUser = new MASPlugin.MASUser();
-            masUser.logoutUser(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MASUser.logoutUser()';
+        var masUser = new MASPlugin.MASUser();
+        masUser.logoutUser(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     createActionButton('MASUser.isAuthenticated()', function() {
-            var name = 'MASUser.isAuthenticated()';
-            var masUser = new MASPlugin.MASUser();
-            masUser.isAuthenticated(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' Failed: ' + result);
-            }
-        });
+        var name = 'MASUser.isAuthenticated()';
+        var masUser = new MASPlugin.MASUser();
+        masUser.isAuthenticated(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' Failed: ' + result);
+        }
+    });
 
     //MASDevice
     createActionButton('MASDevice.deregister()', function() {
-            var name = 'MASDevice.deregister()';
-            var masDevice = new MASPlugin.MASDevice();
-            masDevice.deregister(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' - Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' - Failed: ' + error);
-            }
-        });
+        var name = 'MASDevice.deregister()';
+        var masDevice = new MASPlugin.MASDevice();
+        masDevice.deregister(startSuccessHandler, errorHandler);
 
-     createActionButton('MASDevice.isDeviceRegistered()', function() {
-            var name = 'MASDevice.isDeviceRegistered()';
-            var masDevice = new MASPlugin.MASDevice();
-            masDevice.isDeviceRegistered(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' - Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' - Failed: ' + error);
-            }
-        });
+        function startSuccessHandler(result) {
+            console.log(name + ' - Success: ' + result);
+        }
 
-     createActionButton('MASDevice.resetLocally()', function() {
-            var name = 'MASDevice.resetLocally()';
-            var masDevice = new MASPlugin.MASDevice();
-            masDevice.resetLocally(startSuccessHandler, errorHandler);
-            function startSuccessHandler(result) {
-                console.log(name + ' - Success: ' + result);
-            }
-            function errorHandler(error) {
-                console.log(name + ' - Failed: ' + error);
-            }
-        });
+        function errorHandler(error) {
+            console.log(name + ' - Failed: ' + error);
+        }
+    });
+
+    createActionButton('MASDevice.isDeviceRegistered()', function() {
+        var name = 'MASDevice.isDeviceRegistered()';
+        var masDevice = new MASPlugin.MASDevice();
+        masDevice.isDeviceRegistered(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' - Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' - Failed: ' + error);
+        }
+    });
+
+    createActionButton('MASDevice.resetLocally()', function() {
+        var name = 'MASDevice.resetLocally()';
+        var masDevice = new MASPlugin.MASDevice();
+        masDevice.resetLocally(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log(name + ' - Success: ' + result);
+        }
+
+        function errorHandler(error) {
+            console.log(name + ' - Failed: ' + error);
+        }
+    });
 
 
 
     createActionButton('Valid Registration Type', function() {
-                       
-                       var MAS = new MASPlugin.MAS();
-                       MAS.deviceRegistrationType(null,null,'0');
-                       MAS.start(startSuccessHandler,errorHandler);
-                       
-                       function startSuccessHandler(result)
-                       {
-                       console.log("Valid Registration Type Test Pass");
-                       
-                       }
-                       function errorHandler(error)
-                       {
-                       console.log("Valid Registration Type Test Fail");
-                       
-                       }
-                       });
-    
+
+        var MAS = new MASPlugin.MAS();
+        MAS.deviceRegistrationType(null, null, '0');
+        MAS.start(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log("Valid Registration Type Test Pass");
+
+        }
+
+        function errorHandler(error) {
+            console.log("Valid Registration Type Test Fail");
+
+        }
+    });
+
     createActionButton('Deregister Device', function() {
-                       
-                       var MASDevice = new MASPlugin.MASDevice();
-                       MASDevice.deregister(deregisterSuccessHandler,deregisterErrorHandler);
-                       function deregisterSuccessHandler(result)
-                       {
-                       console.log("Deregister Device Success");
-                       
-                       }
-                       function deregisterErrorHandler(error)
-                       {
-                       console.log("Deregister Device Failure");
-                       
-                       }
-                       });
-    
+
+        var MASDevice = new MASPlugin.MASDevice();
+        MASDevice.deregister(deregisterSuccessHandler, deregisterErrorHandler);
+
+        function deregisterSuccessHandler(result) {
+            console.log("Deregister Device Success");
+
+        }
+
+        function deregisterErrorHandler(error) {
+            console.log("Deregister Device Failure");
+
+        }
+    });
+
     createActionButton('Invalid Registration Type', function() {
-                       
-                       var MAS = new MASPlugin.MAS();
-                       MAS.deviceRegistrationType(null,null,'6');
-                       MAS.start(startSuccessHandler,errorHandler);
-                       
-                       function startSuccessHandler(result)
-                       {
-                       console.log("Invalid Registration Type Fail");
-                       
-                       }
-                       function errorHandler(error)
-                       {
-                       console.log("Invalid Registration Type Pass");
-                       
-                       }
-                       });
-    
+
+        var MAS = new MASPlugin.MAS();
+        MAS.deviceRegistrationType(null, null, '6');
+        MAS.start(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log("Invalid Registration Type Fail");
+
+        }
+
+        function errorHandler(error) {
+            console.log("Invalid Registration Type Pass");
+
+        }
+    });
+
     createActionButton('Valid config filename', function() {
-                       
-                       var MAS = new MASPlugin.MAS();
-                       MAS.configFileName(null,null,"msso_config");
-                       MAS.start(startSuccessHandler,errorHandler);
-                       
-                       function startSuccessHandler(result)
-                       {
-                       console.log("Valid config filename Pass");
-                       
-                       }
-                       function errorHandler(error)
-                       {
-                       console.log("Valid config filename Fail");
-                       
-                       }
-                       });
-    
-    
+
+        var MAS = new MASPlugin.MAS();
+        MAS.configFileName(null, null, "msso_config");
+        MAS.start(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log("Valid config filename Pass");
+
+        }
+
+        function errorHandler(error) {
+            console.log("Valid config filename Fail");
+
+        }
+    });
+
+
     createActionButton('Invalid config filename', function() {
-                       
-                       var MAS = new MASPlugin.MAS();
-                       MAS.configFileName(null,null,"msso_config1");
-                       MAS.start(startSuccessHandler,errorHandler);
-                       
-                       function startSuccessHandler(result)
-                       {
-                       console.log("Invalid config filename Fail");
-                       
-                       }
-                       function errorHandler(error)
-                       {
-                       console.log("Invalid config filename Pass");
-                       
-                       }
-                       });
-    
+
+        var MAS = new MASPlugin.MAS();
+        MAS.configFileName(null, null, "msso_config1");
+        MAS.start(startSuccessHandler, errorHandler);
+
+        function startSuccessHandler(result) {
+            console.log("Invalid config filename Fail");
+
+        }
+
+        function errorHandler(error) {
+            console.log("Invalid config filename Pass");
+
+        }
+    });
+
 };
